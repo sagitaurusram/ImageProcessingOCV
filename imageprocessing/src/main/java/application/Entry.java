229ebc9b41097ccc90d24378eb722bb0ded2e00f
sriram madavswamy <sagitaurus.ram@gmail.com>
2015-12-01 22:55:26 +0530

    after open cv install

--- imageprocessing/src/main/java/application/Entry.java
+++ imageprocessing/src/main/java/application/Entry.java
@@ -1,10 +1,18 @@
 package application;
 
+
+import org.opencv.core.Core;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.core.Scalar;
+
 public class Entry {
-
+	
+	static { System.loadLibrary(Core.NATIVE_LIBRARY_NAME);}
 	public static void main(String[] args) {
-		System.out.println("Welcome ");
-
+		Mat m = new Mat(5,10,CvType.CV_8UC1, new Scalar(0));
+		System.out.println("OpenCV mat : "+m.dump());
+		
 	}
 
 }
