2b63475a18688c7ce6ef2ab291fb01f078ed56bc
sriram madavswamy <sagitaurus.ram@gmail.com>
2015-12-01 23:58:16 +0530

    gui related

--- imageprocessing/src/main/java/gui/FXcontroller.java
+++ imageprocessing/src/main/java/gui/FXcontroller.java
@@ -2,24 +2,26 @@ package gui;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
 public class FXcontroller {
-@FXML
-private Button loadImage;
+	@FXML
+	private Button loadImage;
 
-@FXML
-private Button filterImage;
+	@FXML
+	private Button filterImage;
 
-@FXML
-private ImageView imageFrame;
+	@FXML
+	private ImageView imageFrame;
 
-@FXML
-public void loadImage(){
-	System.out.println("load image button pressed");
-}
-@FXML
-public void filterImage(){
-	
-}
+	@FXML
+	public void loadImage() {
+		this.imageFrame.setImage(new Image("/images/colourWheel.jpg"));
+	}
+
+	@FXML
+	public void filterImage() {
+
+	}
 }
